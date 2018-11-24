3ba6771bc677d8f4111e0e98da3ba334d7459a22
Ramil <lolik111110@gmail.com>
2018-11-24 23:55:47 +0300

    hotfix

--- run_classifier.py
+++ run_classifier.py
@@ -706,7 +706,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
                     ground_truth = tf.log1p(tf.clip_by_value(tf.cast(label_ids, tf.float32), 1e-8, 1e+30))
                     predictions = tf.log1p(tf.clip_by_value(tf.squeeze(logits), 1e-8, 1e+30))
                     return {
-                        "loss": tf.metrics.mean(per_example_loss),
+                        "eval_loss": tf.metrics.mean(per_example_loss),
                         "another_loss": tf.metrics.mean_squared_error(ground_truth, predictions)
                     }
 
